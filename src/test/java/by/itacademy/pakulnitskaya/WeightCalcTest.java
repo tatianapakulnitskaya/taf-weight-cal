328eb41a73f8cd166aab36e76bb8ad20e0d39686
Tatiana Pakulnitskaya <49642178+tatianapakulnitskaya@users.noreply.github.com>
2023-11-08 23:39:09 +0300

    Update WeightCalcTest.java

--- src/test/java/by/itacademy/pakulnitskaya/WeightCalcTest.java
+++ src/test/java/by/itacademy/pakulnitskaya/WeightCalcTest.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static java.lang.Thread.sleep;
-
 public class WeightCalcTest {
 
     @Test
