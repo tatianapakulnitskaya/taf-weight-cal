0fa266effc4aaef94669d1ece870e4b2943e5f92
Tatiana_Pakulnitskaya <takovenya@gmail.com>
2023-11-11 16:01:51 +0300

    Add tests

--- src/test/java/by/itacademy/pakulnitskaya/WeightCalcTest.java
+++ src/test/java/by/itacademy/pakulnitskaya/WeightCalcTest.java
@@ -12,7 +12,7 @@ import static java.lang.Thread.sleep;
 public class WeightCalcTest {
 
     @Test
-    public void testFormWithEmptyValues() throws InterruptedException {
+    public void testFormWithEmptyValues() {
         //Starting a driver session
         WebDriver driver = new ChromeDriver();
         //Navigating to a web page
@@ -34,5 +34,82 @@ public class WeightCalcTest {
         String expectedResult = "Не указано имя.\n" + "Рост должен быть в диапазоне 50-300 см.\n" + "Вес должен быть в диапазоне 3-500 кг.\n" + "Не указан пол.";
         //Check whether Actual and Expected results are match
         Assertions.assertEquals(actualResult, expectedResult);
+        //End the session
+        driver.quit();
+    }
+
+    @Test
+    public void testFormWithEmptyNameHeightWeight() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String inputRadioGenderXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By inputRadioGenderBy = By.xpath(inputRadioGenderXpath);
+        WebElement inputRadioGenderElement = driver.findElement(inputRadioGenderBy);
+        inputRadioGenderElement.click();
+        String inputButtonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By inputButtonCalculateBy = By.xpath(inputButtonCalculateXpath);
+        WebElement inputButtonCalculateElement = driver.findElement(inputButtonCalculateBy);
+        inputButtonCalculateElement.click();
+        String textEmptyValueAlertXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td";
+        By textEmptyValueAlertBy = By.xpath(textEmptyValueAlertXpath);
+        WebElement testEmptyValueAlertElement = driver.findElement(textEmptyValueAlertBy);
+        String actualResult = testEmptyValueAlertElement.getText();
+        String expectedResult = "Не указано имя.\n" + "Рост должен быть в диапазоне 50-300 см.\n" + "Вес должен быть в диапазоне 3-500 кг.";
+        Assertions.assertEquals(actualResult, expectedResult);
+        driver.quit();
+    }
+
+    @Test
+    public void testFormWithEmptyNameHeight() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String inputFieldWeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputFieldWeightBy = By.xpath(inputFieldWeightXpath);
+        WebElement inputFieldWeightElement = driver.findElement(inputFieldWeightBy);
+        inputFieldWeightElement.sendKeys("100");
+        String inputRadioGenderXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By inputRadioGenderBy = By.xpath(inputRadioGenderXpath);
+        WebElement inputRadioGenderElement = driver.findElement(inputRadioGenderBy);
+        inputRadioGenderElement.click();
+        String inputButtonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By inputButtonCalculateBy = By.xpath(inputButtonCalculateXpath);
+        WebElement inputButtonCalculateElement = driver.findElement(inputButtonCalculateBy);
+        inputButtonCalculateElement.click();
+        String textEmptyValueAlertXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td";
+        By textEmptyValueAlertBy = By.xpath(textEmptyValueAlertXpath);
+        WebElement testEmptyValueAlertElement = driver.findElement(textEmptyValueAlertBy);
+        String actualResult = testEmptyValueAlertElement.getText();
+        String expectedResult = "Не указано имя.\n" + "Рост должен быть в диапазоне 50-300 см.";
+        Assertions.assertEquals(actualResult, expectedResult);
+        driver.quit();
+    }
+
+    @Test
+    public void testFormWithEmptyName() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        String inputFieldHeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputFieldHeightBy = By.xpath(inputFieldHeightXpath);
+        WebElement inputFieldHeightElement = driver.findElement(inputFieldHeightBy);
+        inputFieldHeightElement.sendKeys("50");
+        String inputFieldWeightXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputFieldWeightBy = By.xpath(inputFieldWeightXpath);
+        WebElement inputFieldWeightElement = driver.findElement(inputFieldWeightBy);
+        inputFieldWeightElement.sendKeys("100");
+        String inputRadioGenderXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By inputRadioGenderBy = By.xpath(inputRadioGenderXpath);
+        WebElement inputRadioGenderElement = driver.findElement(inputRadioGenderBy);
+        inputRadioGenderElement.click();
+        String inputButtonCalculateXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By inputButtonCalculateBy = By.xpath(inputButtonCalculateXpath);
+        WebElement inputButtonCalculateElement = driver.findElement(inputButtonCalculateBy);
+        inputButtonCalculateElement.click();
+        String textEmptyValueAlertXpath = "/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[1]/td";
+        By textEmptyValueAlertBy = By.xpath(textEmptyValueAlertXpath);
+        WebElement testEmptyValueAlertElement = driver.findElement(textEmptyValueAlertBy);
+        String actualResult = testEmptyValueAlertElement.getText();
+        String expectedResult = "Не указано имя.";
+        Assertions.assertEquals(actualResult, expectedResult);
+        driver.quit();
     }
 }
